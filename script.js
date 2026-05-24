6b6a83fe35c5047f7b49d16f24a6890d8e86209c
ahmed26ar <fx62172@gmail.com>
2026-05-25 00:19:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("الموقع يعمل بنجاح!");
+function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
+}
+
+function changeLang() {
+    const isRtl = document.documentElement.dir === 'rtl';
+    document.documentElement.dir = isRtl ? 'ltr' : 'rtl';
+    document.documentElement.lang = isRtl ? 'en' : 'ar';
+    alert("سيتم تغيير اللغة قريباً في تحديث الكود!");
+}
